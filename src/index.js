f6e949717f72b4638953ea41c0ccf543be0d2e28
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2021-02-05 18:44:27 +0200

    fead: realized functions min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+    const arr = array.sort((a, b) => a - b);
+    return arr[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+    const arr = array.sort((a, b) => a - b);
+    return arr[arr.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array) return 0;
+    if (array.length === 0) return 0;
+    const arr = array.reduce((acc, current) => acc + current, 0) / array.length;
+    return arr;
+};
